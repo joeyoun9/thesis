9d98529552766b7f2708f55daabbc601ecb6cb79
Joe Young <joeyoungwx@gmail.com>
2012-02-22 00:13:45 -0700

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='thesis',
+      version='0.01',
+      description="Joe Young\'s Thesis code",
+      author='Joe Young',
+      author_email='joe.young@utah.edu',
+      url='http://www.jsyoung.us/code/',
+      packages=['thesis','thesis.rass',
+	'thesis.ceil','thesis.dem','thesis.rwp',
+	'thesis.isfs','thesis.sonde'],
+     )
+
