32172cc8650b43487657614bbb6b789a22b34a2c
Joe Young <joeyoungwx@gmail.com>
2012-05-18 14:46:36 -0600

    Created a PCAPS package

---
+++ thesis/pcaps/__init__.py
@@ -0,0 +1,30 @@
+"""
+	Methods and data for the PCAPS projects
+"""
+
+all = ['compress','tools']
+
+
+
+def iop(num):
+	if num == 1:
+		return (s2t('2010120112UTC','%Y%m%d%H%Z'),s2t('2010120702UTC','%Y%m%d%H%%Z'))
+	elif num == 2:
+		return (s2t('2010120712UTC','%Y%m%d%H%Z'),s2t('2010121015UTC','%Y%m%d%H%%Z'))
+	elif num == 3:
+		return (s2t('2010121212UTC','%Y%m%d%H%Z'),s2t('2010121421UTC','%Y%m%d%H%%Z'))
+	elif num == 4:
+		return (s2t('2010120100UTC','%Y%m%d%H%Z'),s2t('2010120915UTC','%Y%m%d%H%%Z'))
+	elif num == 5:
+		return (s2t('2010120100UTC','%Y%m%d%H%Z'),s2t('2010120915UTC','%Y%m%d%H%%Z'))
+	elif num == 6:
+		return (s2t('2010120100UTC','%Y%m%d%H%Z'),s2t('2010120915UTC','%Y%m%d%H%%Z'))
+	elif num == 7:
+		return (s2t('2010120100UTC','%Y%m%d%H%Z'),s2t('2010120915UTC','%Y%m%d%H%%Z'))
+	elif num == 8:
+		return (s2t('2010120100UTC','%Y%m%d%H%Z'),s2t('2010120915UTC','%Y%m%d%H%%Z'))
+	elif num == 9:
+		return (s2t('2010120100UTC','%Y%m%d%H%Z'),s2t('2010120915UTC','%Y%m%d%H%%Z'))
+	elif num == 10:
+		return (s2t('2010120100UTC','%Y%m%d%H%Z'),s2t('2010120915UTC','%Y%m%d%H%%Z'))
+	
