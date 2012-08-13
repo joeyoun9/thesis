a4f6ca2abf1c4c1c8675163d75e2f1b5eaf6090b
Joe Young <joeyoungwx@gmail.com>
2012-08-13 17:25:53 -0600

    3

--- thesis/tools/sounding.py
+++ thesis/tools/sounding.py
@@ -44,7 +44,7 @@ def c2t (t,v,var,conversions=default_time_conversions,z=False,offset=False,color
     
     
     '''
-    if not z or z[0]:
+    if not z.any():
         skewz = lambda x,y: x # create skewz as a meaningless function
         z = [0 for x in range(len(v))] # create a dummy Z
     else:
