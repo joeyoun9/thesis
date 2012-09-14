f8002a4cf9aabb3e9306851cb3512b68daff2e5e
Joe Young <joeyoungwx@gmail.com>
2012-09-14 13:53:00 -0600

    2

--- thesis/compress/sonde/ncar_raw.py
+++ thesis/compress/sonde/ncar_raw.py
@@ -45,6 +45,7 @@ def read(files,save):
 		l=0 #keeps track of the current line
 		f = open(f)
 		for line in f:
+			print line
 			if not line[10]=='S': continue #dont record pre-launch
 			line  = line.split()
 			if not line[1] == 'S00': continue #not satisfactory
