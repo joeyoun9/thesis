9d9f9cee3b13abacc9b795f1fc80f771c3853935
jsyoung <joeyoungwx@gmail.com>
2013-02-18 23:20:54 -0600

    Added a copy method, undoing the previous numpy array inheritance

--- thesis/tools/core/objects.py
+++ thesis/tools/core/objects.py
@@ -9,9 +9,9 @@ Muto.
 This is not meant to be included in the bundle, it is an internal element.
 
 '''
-import numpy
+import copy
 
-class core_object(numpy.array):
+class core_object():
     ''' 
     A simple object which possesses the ability to have its attributes
     manipulated with via the dictionary structure as well as the attribute
@@ -22,6 +22,9 @@ class core_object(numpy.array):
     Note that there is a catch for all situations here, and therefore this 
     is unlikely to fail. Hopefully this does not cause any undesired operation.
     '''
+    def __init__(self):
+        # nothing to do here at this time
+        pass
     def __getitem__(self, key):
         try:
             return self.__dict__[key]
@@ -30,6 +33,13 @@ class core_object(numpy.array):
             self.__dict__[key] = None
     def __setitem__(self, key, value):
         self.__dict__[key] = value
+
+    def copy(self):
+        '''
+        Return a duplicate of this object. Deep copies are mandated because of
+        the recursive object property within
+        '''
+        return copy.deepcopy(self)
 
 
 '''
