2c1b76d5edc57e84621537756be9ca6d7c33852e
jsyoung <joeyoungwx@gmail.com>
2013-05-01 22:09:22 -0500

    forgot to return on initialize

--- thesis/tools/core/objects.py
+++ thesis/tools/core/objects.py
@@ -88,6 +88,7 @@ class CoreObject(object):
     def __iter__(self):
         self._current = 0
         self._max = len(self.time)
+        return self
     def next(self):
         if self._current > self._max:
             raise StopIteration
