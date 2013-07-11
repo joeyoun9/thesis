aa80780e1e073daa3b419ee9aa1f1ac59503eaca
jsyoung <joeyoungwx@gmail.com>
2013-07-10 23:21:02 -0500

    testing new git setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='thesis',
-      version='1.6',
+      version='1.6.2',
       description="Joe Young\'s Thesis code",
       author='Joe Young',
       author_email='joe.young@utah.edu',
